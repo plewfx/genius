fb2a452ed62e027b28361059241e124a86b5bf66
Ali <plewffy@gmail.com>
2023-10-27 23:14:56 +0600

    remove console.logs

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,4 @@
-import { fetchSongLyrics } from "@utils";
-
 export default async function Home() {
-  const search = await fetchSongLyrics();
-  console.log(search);
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
